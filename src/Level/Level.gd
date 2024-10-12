e343b2910974d2f0287fb502766dcc67f193be8f
Nicolas Busser <nicolas.busser@lacollection.io>
2024-10-12 22:13:20 +0900

    add LevelState

--- src/Level/Level.gd
+++ src/Level/Level.gd
@@ -3,30 +3,27 @@ extends Node
 signal end_of_level
 signal game_over
 
-var level_number
-var nb_coins
-
 @onready var hud = $UI/HUD
 @onready var timer: Timer = $Timer
 
-var level_data: LevelData
+var level_state: LevelState
 
 
 func _ready():
 	assert(
-		level_number != null and nb_coins != null, "init must be called before creating Level scene"
+		level_state, "init must be called before creating Level scene"
 	)
-	hud.set_level_number(level_number)
-	hud.set_coins(nb_coins)
-	hud.set_level_name(level_data.name)
+	hud.set_level_number(level_state.level_number)
+	hud.set_coins(level_state.nb_coins)
+	hud.set_level_name(level_state.level_data.name)
 
-	self.timer.start(self.level_data.timer_duration)
+	timer.start(level_state.level_data.timer_duration)
 
 
 func init(level_data_p: LevelData, level_number, nb_coins):
-	self.level_data = level_data_p
-	self.level_number = level_number
-	self.nb_coins = nb_coins
+	level_state = LevelState.new(
+		level_number, level_data_p, nb_coins
+	)
 
 
 func _on_Timer_timeout():
---
+++ src/Level/LevelState.gd
@@ -0,0 +1,16 @@
+extends Resource
+
+class_name LevelState
+
+# Represents the state of the level
+# Carries the level confiruation but also holds game state information
+
+var level_number: int = 0
+var level_data: LevelData # Config of the level
+
+var nb_coins: int = 0 # Coins collected from the beggining of game
+
+func _init(level_number_p: int, level_data_p: LevelData, nb_coins_p: int):
+    self.level_number = level_number_p
+    self.level_data = level_data_p
+    self.nb_coins = nb_coins_p
