28b8860d48819f4bb177a9f8e8363ac3f8098e99
Nicolas Busser <nicolas.busser@lacollection.io>
2024-10-12 22:32:40 +0900

    add fadein

--- src/HUD/HUD.gd
+++ src/HUD/HUD.gd
@@ -5,20 +5,26 @@ class_name HUD
 @onready var level_number_label = $VBoxContainer/VBoxContainer/LevelNumber/LevelNumberValue
 @onready var coins_label = $VBoxContainer/VBoxContainer/CoinNumber/CoinNumberValue
 @onready var level_name_label = $VBoxContainer/CenterContainer/LevelNameValue
+@onready var fadein_pane = $FadeinPane
 
 var level_name: set = set_level_name
-func set_level_name(value: String):
+func set_level_name(value: String) -> void:
 	level_name_label.text = value
 
 var nb_coins: set = set_nb_coins
-func set_nb_coins(value: int):
+func set_nb_coins(value: int) -> void:
 	coins_label.text = str(value)
 
 var level_number: set = set_level_number
-func set_level_number(value: int):
+func set_level_number(value: int) -> void:
 	level_number_label.text = str(value)
 
-func init(level_state: LevelState):
+func init(level_state: LevelState) -> void:
 	level_name = level_state.level_data.name
 	level_number = level_state.level_number
 	nb_coins = level_state.nb_coins
+
+func _ready() -> void:
+	# Fadein animation
+	fadein_pane.visible = 1
+	create_tween().tween_property(fadein_pane, "modulate", Color.TRANSPARENT, 0.7)
